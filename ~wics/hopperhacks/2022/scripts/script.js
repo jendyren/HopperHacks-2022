97305b49a134c6fb074360b95906508fb69de2b3
jendy <53535722+jendyren@users.noreply.github.com>
2021-12-26 20:27:37 +0000

    responsive navbar

--- ~wics/hopperhacks/2022/scripts/script.js
+++ ~wics/hopperhacks/2022/scripts/script.js
@@ -1,21 +1,35 @@
-var scrollpos = window.scrollY;
-var header = document.getElementById("header");
-var windowheight = window.innerHeight;
-var headerheight = document.getElementById("header").offsetHeight;
-
-function add_class_on_scroll() {
-  header.classList.add("fixed");
-}
-
-function remove_class_on_scroll() {
-  header.classList.remove("fixed");
-}
-
-window.addEventListener('scroll', function() {
-  scrollpos = window.scrollY;
-  if (scrollpos > (windowheight - headerheight)) {
-    add_class_on_scroll();
-  } else {
-    remove_class_on_scroll();
+//hamburger menu
+var menu = document.getElementById("menu-btn")
+var menu_items = document.getElementById("menu-items")
+menu.addEventListener('click', () => {
+  if(menu.classList.contains("is-active")){
+    menu.classList.remove("is-active")
+    menu_items.classList.remove("active")
   }
-});
+  else {
+    menu.classList.add("is-active")
+    menu_items.classList.add("active")
+  }
+})
+
+// var scrollpos = window.scrollY;
+// var header = document.getElementById("header");
+// var windowheight = window.innerHeight;
+// var headerheight = document.getElementById("header").offsetHeight;
+
+// function add_class_on_scroll() {
+//   header.classList.add("fixed");
+// }
+
+// function remove_class_on_scroll() {
+//   header.classList.remove("fixed");
+// }
+
+// window.addEventListener('scroll', function() {
+//   scrollpos = window.scrollY;
+//   if (scrollpos > (windowheight - headerheight)) {
+//     add_class_on_scroll();
+//   } else {
+//     remove_class_on_scroll();
+//   }
+// });
